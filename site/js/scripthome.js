d9e640f6deb7d9fdbb0d06534beee141b10c7d37
GustavoQuaresma <gustavo.costa@bandtec.com.br>
2021-05-13 13:56:44 -0300

    adsada

--- site/js/scripthome.js
+++ site/js/scripthome.js
@@ -1,11 +1,20 @@
 window.onscroll = function () {
+    if (window.scrollY < slogan.offsetHeight) {
+        cabecalho.style = " transform: translateY(-100%) ; ";
+        jogosli.classList.remove('destaque');
+        homeli.classList.add('destaque');
+        graficoli.classList.remove('destaque');
+    }
     if (window.scrollY >= slogan.offsetHeight) {
         cabecalho.style = " transform: translateY(0) ; ";
-    
+        jogosli.classList.add('destaque');
+        homeli.classList.remove('destaque');
+        graficoli.classList.remove('destaque');
     }
-    else {
-        cabecalho.style = "";
-        
+    if (window.scrollY >= home.offsetHeight) {
+        graficoli.classList.add('destaque');
+        jogosli.classList.remove('destaque');
+        homeli.classList.remove('destaque');
     }
 }
 
